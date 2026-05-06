e6b4fdb508539dec75e847739fca54c968b8cda7
Johannes Hutapea <v2rayvvip@gmail.com>
2026-05-06 13:56:14 +0700

    Cleanup: remove unrelated files from repository

--- npm24782046_iet_2026/main_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Report
-
-@admin.register(Report)
-class ReportAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'category', 'created_at')
-    list_filter = ('status',)
-    search_fields = ('title',)
